94d0ca99d746e67615f9146e4e6c4532b63d71f0
Elena-Gallego <elenagallego.front@gmail.com>
2020-08-10 16:17:10 +0200

    added docs

---
+++ docs/precache-manifest.68fa6ea27ca416b08eb1635c6e4e4a89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da67c764ecc9ee0457a542c1a56ac10c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f179e9604a69a7d642dd",
+    "url": "./static/css/main.6a09223c.chunk.css"
+  },
+  {
+    "revision": "98efcd7c9b0a10c112f0",
+    "url": "./static/js/2.d760aea4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.d760aea4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f179e9604a69a7d642dd",
+    "url": "./static/js/main.41e52fef.chunk.js"
+  },
+  {
+    "revision": "a0e6f4cee20c0c79b2e6",
+    "url": "./static/js/runtime-main.8bb1f59a.js"
+  }
+]);
